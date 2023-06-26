0b73cf5b4da4c7914b7e174a35f939379184cfd2
MaiManh15 <sorryfornoob15@gmail.com>
2023-06-27 00:36:35 +0700

    Làm Home Navigation tĩnh
    Thiếu icon với chưa có hover

--- client/src/Assets/Component/Navigation Bar/Home Navigation/HomeNavigation.js
+++ client/src/Assets/Component/Navigation Bar/Home Navigation/HomeNavigation.js
@@ -1,9 +1,49 @@
-import React from "react";
+
+import React from "react";  
+import { ReactComponent as Logo } from "../../../Image/Logo.svg"
+import {Navigation_Wrapper,
+        Navigation_Logo,
+        Navigation_Subtitle_Wrapper,
+        Navigation_Subtitle_ul,
+        Navigation_Subtitle_li,
+        Navigation_Icon_Wrapper,
+        Navigation_Icon,
+        Item_Number } from "./HomeNavigation_Styled";
 
 export default function HomeNavigation() {
+    
     return (
-        <div>
-            <p>This is Home navigation</p>
-        </div>
+        <Navigation_Wrapper>
+
+            <Navigation_Logo>
+                <Logo style={{cursor:"pointer"}}></Logo>
+            </Navigation_Logo>
+
+            <Navigation_Subtitle_Wrapper>
+                
+                <Navigation_Subtitle_ul>
+
+                    <Navigation_Subtitle_li style={{color: "#7e9e2c"}}>Home</Navigation_Subtitle_li>
+                    <Navigation_Subtitle_li>Shop</Navigation_Subtitle_li>
+                    <Navigation_Subtitle_li>Products</Navigation_Subtitle_li>
+                    <Navigation_Subtitle_li>Blog</Navigation_Subtitle_li>
+                    <Navigation_Subtitle_li>Page</Navigation_Subtitle_li>
+
+                </Navigation_Subtitle_ul>
+
+            </Navigation_Subtitle_Wrapper>
+
+            <Navigation_Icon_Wrapper>
+
+                    <Item_Number>1</Item_Number>
+                    <Navigation_Icon></Navigation_Icon>
+                    <Item_Number>0</Item_Number>
+                    <Navigation_Icon></Navigation_Icon>
+                    <Navigation_Icon></Navigation_Icon>
+                    <Navigation_Icon></Navigation_Icon>
+                    
+            </Navigation_Icon_Wrapper>
+
+        </Navigation_Wrapper>
     )
 }
---
+++ client/src/Assets/Component/Navigation Bar/Home Navigation/HomeNavigation_Styled.js
@@ -0,0 +1,67 @@
+
+import styled from "styled-components"
+
+export const Navigation_Wrapper=styled.div`
+    width:80%;
+    height: 100px;
+    margin: 40px auto;
+    display: flex;
+`;
+
+export const Navigation_Logo=styled.div`
+    width:25%;
+    
+    &>svg{
+        height:100px;
+        float: left;
+    }
+`;
+
+export const Navigation_Subtitle_Wrapper=styled.div`
+    width:50%;
+`;
+
+export const Navigation_Subtitle_ul=styled.ul`
+    padding:0;
+    margin: 0;
+    line-height:100px;
+`;
+
+export const Navigation_Subtitle_li=styled.li`
+    list-style: none;
+    display: inline-block;
+    font-family: sans-serif;
+    font-size: 20px;
+    font-weight: 700;
+    margin: 0px 20px 0 20px;
+    cursor:pointer;
+`;
+
+export const Navigation_Icon_Wrapper=styled.div`
+    width:25%;
+    line-height:100px;
+`;
+
+export const Navigation_Icon=styled.div`
+    width: 22px;
+    height: 22px;
+    background: pink;
+    margin: 40px 0px 0 20px;
+    display: inline-block;
+    float: right;
+    cursor:pointer;
+`;
+
+export const Item_Number=styled.div`
+    background: #406b0f;
+    width: 28px;
+    height: 28px;
+    display: inline-block;
+    float: right;
+    margin: 28px 0px 0px;
+    border-radius: 50%;
+    line-height: 28px;
+    color: white;
+    font-size: 16px;
+    font-weight: 600;
+`;
