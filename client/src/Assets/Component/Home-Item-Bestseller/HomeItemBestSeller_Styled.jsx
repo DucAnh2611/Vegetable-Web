ca50b146d84af673853911677d30633013429b81
TienDung2002 <101706065+TienDung2002@users.noreply.github.com>
2023-06-27 21:53:51 +0700

    commit
    cmt1

--- client/src/Assets/Component/Home-Item-Bestseller/HomeItemBestSeller_Styled.jsx
+++ client/src/Assets/Component/Home-Item-Bestseller/HomeItemBestSeller_Styled.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 export const BestSeller_Wrap = styled.div`
-    background: var(--Third_Green);
+    background: var(--Primary_White);
     height: 600px;
     width: 100%;
     padding: 0 15px;
@@ -10,8 +10,36 @@ export const BestSeller_Wrap = styled.div`
 export const AllProduct_wrap = styled.div`
     width: 100%;
     height: auto;
+
+    h1 {
+        font-weight: 500;
+        margin-bottom: 10px;
+    }
 `;
 
-export const Filter_category = styled.div`
+export const Filter_category_wrap = styled.div`
+    margin: 0 auto;
+    height: auto;
+    width: 500px;
+`;
+
+export const Filter_category_list = styled.ul`
     display: flex;
+    justify-content: space-around;
+    font-size: 17px;
+    margin-bottom: 50px;
+
+    li:first-child{
+        padding: 0 10px 0 0;
+    }
+
+    li {
+        padding: 0 12px;
+    }
+
+    li:hover {
+        cursor: pointer;
+        color: var(--Fifth_Green);
+        transition: 0.2s;
+    }
 `;
