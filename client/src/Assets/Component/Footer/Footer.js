ce86de179709ed4d8dfa46d47417054c79cf1f74
DucAnh2611 <117668851+DucAnh2611@users.noreply.github.com>
2023-06-29 17:14:15 +0700

    Hoan thanh layout

--- client/src/Assets/Component/Footer/Footer.js
+++ client/src/Assets/Component/Footer/Footer.js
@@ -1,5 +1,6 @@
 
 import React from "react";
+import { ReactComponent as Logo } from "../../Image/SVG/horizon_logo.svg";
 
 export default function Footer() {
     return (
@@ -9,7 +10,7 @@ export default function Footer() {
             <div>
 
                 <div>
-                    <span>Logo</span>
+                    <span><Logo/></span>
                 </div>
 
                 <div>
